3fabc119b0ec9443971e712c06b977f04faf2d6e
mingxun <mingxun@ubuntu.(none)>
2012-12-11 21:37:43 -0800

    using new website

--- web.rb
+++ web.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'erb'
 
 get '/' do
-	"HELLO WORLD"
+    musicfile = File.open("views/music.html", "rb")
+    @music_html = musicfile.read
+    @music_html = @music_html.gsub(/\n/," ") 
+    erb :homepage
 end
+
+
+
